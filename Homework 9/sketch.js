19d0784714b8eb53fd87441a88ac70eb77afc3f1
Tegan Keller <teganraekeller@gmail.com>
2022-04-01 16:26:46 -0600

    hw9+

---
+++ Homework 9/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(123,23,129);
+  describe('canvas with darkest charcoal grey background')
+  ellipse(200, 120, 100, 100);
+  line(190, 140, 210, 140);
+  rect(150, 240, 40, 140, 50);
+  rect(210, 240, 40, 140, 50);
+rect(130, 180, 140, 30, 50);
+  rect(250, 180, 30, 140, 50);
+  rect(120, 180, 30, 140, 50);
+  rect(150, 170, 100, 140, 50);
+  fill(0, 0, 0);
+
+point(190, 100);
+point(210, 100);
+strokeWeight(4);
+  
+textSize(32);
+text('This is human.', 10, 30);
+  fill(0, 102, 153);
+text('Human is me.', 10, 60);
+fill(0, 102, 153, 51);
+text('Does not look like me', 10, 360);
+text('Tegz', 10, 300);
+  
+}
