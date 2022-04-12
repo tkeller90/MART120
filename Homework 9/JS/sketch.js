b8cb2a1856cb28c9a11dddee5dd23c1af3c4088e
Tegan Keller <teganraekeller@gmail.com>
2022-04-12 08:21:58 -0600

    hw9 fixes

--- Homework 9/JS/sketch.js
+++ Homework 9/JS/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
   background(120,0,110);
   / head /
-fill (100, 0, 50)
+fill (128, 128, 255)
      ellipse(200, 120, 100, 100);
    / mouth straight line /
   fill (100, 0, 50);
@@ -15,35 +15,36 @@ fill (100, 0, 50)
 point(190, 110);
 point(210, 110);
 strokeWeight(2);
-  
-  
-    / if i remove this one, it breaks the code and i don't know why so i just made it a hat?/
-  fill (100, 15, 50);
-  rect(100, 70, 200, 10);
+  /top of hat/
+  fill (255,200, 0)
+  triangle(120, 75, 200, 50, 270, 75);
+    / bottom of hat/
+  fill (255, 200, 0);
+  rect(100, 75, 200, 10);
       / shoulder /
-  fill (100, 15, 50);
+  fill (128, 128, 255);
 rect(130, 180, 140, 30, 30);
      / right arm / 
-  fill (100, 15, 50);
+  fill (128, 128, 255);
   rect(250, 180, 30, 140, 30);
       / left arm / 
-  fill (100, 0, 50);
+  fill (128, 128, 255);
   rect(120, 180, 30, 140, 30);
       / right leg /
-  fill (100, 0, 50);
+  fill (128, 128, 255);
   rect(150, 240, 40, 130);
       / left leg /
-  fill (100, 15, 50);
+  fill (128, 128, 255);
   rect(210, 240, 40, 130);
       / torso / 
-  fill (100, 0, 50);
+  fill (128, 128, 255);
  rect(150, 170, 100, 100, 40);
 
   
 textSize(32);
-  fill(0, 102, 153);
+  fill (50);
 text('The human is me.', 20, 45);
-fill(0, 100, 150, 50);
+fill(50);
 text('Tegan K.', 270, 375);
   
 }
